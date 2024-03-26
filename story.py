074bf15f11a5057c8bf2c92baec74199a43457b6
pharmeasyMarketing <134919459+pharmeasyMarketing@users.noreply.github.com>
2024-03-26 18:13:00 +0530

    Update story.py

--- story.py
+++ story.py
@@ -871,7 +871,7 @@ def save_data_to_mysql(data_list):
         try:
             table = "stories"
             conn = mysql.connector.connect(
-                host="srv1116.hstgr.io",
+                host="srv1399.hstgr.io",
                 user="u914396707_story",
                 password="Hostinger@123.",
                 database="u914396707_story"
